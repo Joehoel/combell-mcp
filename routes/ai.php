f29b5c808391c12b52eaf87952c5b20eee253d98
Joël Kuijper <jmkuijper@outlook.com>
2025-10-21 20:23:19 +0200

    chore: rename ai route

--- routes/ai.php
+++ routes/ai.php
@@ -4,5 +4,6 @@ use App\Mcp\Servers\CombellServer;
 use Laravel\Mcp\Facades\Mcp;
 
 Mcp::local("combell", CombellServer::class);
-Mcp::web("/mcp/combell", CombellServer::class)
+
+Mcp::web("/mcp", CombellServer::class)
     ->middleware(['mcp.auth']);
